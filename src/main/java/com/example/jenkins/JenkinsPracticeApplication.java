b872a8a85fa4b0fac07a37cac2b963bc5219abec
Lokenderbaghel <loke2198@gmail.com>
2025-10-08 21:01:22 +0530

    second commit

--- src/main/java/com/example/jenkins/JenkinsPracticeApplication.java
+++ src/main/java/com/example/jenkins/JenkinsPracticeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsPracticeApplication {
 
-//	 c
+//	 cewfefef
 	
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsPracticeApplication.class, args);
