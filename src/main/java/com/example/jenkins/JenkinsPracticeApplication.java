99a65263209f03d2dfa8e2b84261b91fb06b4e94
Lokenderbaghel <loke2198@gmail.com>
2025-10-09 20:10:20 +0530

    added Dockerfile

--- src/main/java/com/example/jenkins/JenkinsPracticeApplication.java
+++ src/main/java/com/example/jenkins/JenkinsPracticeApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsPracticeApplication {
 
-//	 cewfefef
+//	 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsPracticeApplication.class, args);
+		
+		System.out.println("docker file added");
 	}
 
 }
